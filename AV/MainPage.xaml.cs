6e14e505a9d0d9e0f5bcca11cd1c52e4c9baa0c5
Dennis12278 <dennissilvageneroso@gmail.com>
2026-04-17 19:57:06 -0300

    first commit

--- AV/MainPage.xaml.cs
+++ AV/MainPage.xaml.cs
@@ -9,37 +9,18 @@ public partial class MainPage : ContentPage
 
     private void ConverterParaFahrenheit(object sender, EventArgs e)
     {
-        if (double.TryParse(EntradaTemp.Text, out double celsius))
-        {
-            double fahrenheit = (celsius * 9 / 5) + 32;
-            TemperaturaConvertida.Text = $"{fahrenheit:F2} °F";
-        }
-        else
-        {
-            TemperaturaConvertida.Text = "Valor inválido";
-        }
+    
     }
 
     private void ConverterParaCelsius(object sender, EventArgs e)
     {
-        if (double.TryParse(EntradaTemp.Text, out double fahrenheit))
-        {
-            double celsius = (fahrenheit - 32) * 5 / 9;
-            TemperaturaConvertida.Text = $"{celsius:F2} °C";
-        }
-        else
-        {
-            TemperaturaConvertida.Text = "Valor inválido";
-        }
+      
     }
 
     private void SliderTemp_ValueChanged(object sender, ValueChangedEventArgs e)
     {
         double valor = e.NewValue;
 
-        EntradaTemp.Text = valor.ToString("F0");
-
-        TemperaturaConvertida.Text = $"{valor:F0} °C";
 
         if (valor > 26)
         {
